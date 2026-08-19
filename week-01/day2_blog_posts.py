e3caec6a4d8656ff8d48e473c6932f2019761760
habeebmujtaba5753 <habeebmujtaba5753@gmail.com>
2026-08-19 15:04:27 +0000

    Week 1 Day 2: blog posts list of dicts

---
+++ week-01/day2_blog_posts.py
@@ -0,0 +1,9 @@
+Dic1 = [
+    {"title": "Dictionary 1", "author": "Habeeb", "views": 1},         {"title": "Dictionary 2", "author": "Habeeb1", "views": 2},
+    {"title": "Dictionary 3", "author": "Habeeb2", "views": 3},
+    {"title": "Dictionary 4", "author": "Habeeb3", "views": 4},
+    {"title": "Dictionary 5", "author": "Habeeb4", "views": 5}
+    ]
+    
+for Dic in Dic1:
+    print(Dic)
